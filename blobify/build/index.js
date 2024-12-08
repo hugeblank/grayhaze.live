f60b637258b826bcb59edf8382ddf6dc03872b6c
hugeblank <hugeblank@gmail.com>
2024-12-08 10:08:01 -0800

    fix: no node modules

--- blobify/build/index.js
+++ blobify/build/index.js
@@ -1,9 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const hls_parser_1 = __importDefault(require("hls-parser"));
+const promises_1 = __importDefault(require("fs/promises"));
 const chokidar_1 = __importDefault(require("chokidar"));
-chokidar_1.default.watch("stream").on("all", (e, path) => {
-    console.log(e, path);
+let playlistCache = new Map();
+// add, change, unlink
+promises_1.default.mkdir("stream", {
+    recursive: true
 });
+chokidar_1.default.watch("stream").on("all", (e, path) => __awaiter(void 0, void 0, void 0, function* () {
+    if (e == "add") {
+        if (path.endsWith(".m3u8")) { // Playlist
+            let out = yield promises_1.default.readFile(path);
+            playlistCache.set(path, out);
+            let pl = hls_parser_1.default.parse(out.toString());
+            console.log(pl.segments[0]);
+        }
+        else if (path.endsWith(".ts")) { // MPEG-2 Segment
+        }
+    }
+    else if (e == "change" && path.endsWith(".m3u8")) {
+        let oldFile = playlistCache.get(path);
+        let newFile = yield promises_1.default.readFile(path);
+        playlistCache.set(path, newFile);
+        const diff = newFile === null || newFile === void 0 ? void 0 : newFile.subarray(oldFile === null || oldFile === void 0 ? void 0 : oldFile.length).toString();
+    }
+}));
--- blobify/src/index.ts
+++ blobify/src/index.ts
@@ -1,7 +1,29 @@
-import * as fs from 'fs'
+import hls from 'hls-parser'
+import fs from 'fs/promises'
 import chokidar from 'chokidar'
+import { MediaPlaylist, Segment } from 'hls-parser/types'
+
+let playlistCache: Map<string, Buffer> = new Map()
 
 // add, change, unlink
-chokidar.watch("stream").on("all", (e, path) => {
-    console.log(e, path)
+fs.mkdir("stream", {
+    recursive: true
+})
+chokidar.watch("stream").on("all", async (e: string, path: string) => {
+    if (e == "add") {
+        if (path.endsWith(".m3u8")) { // Playlist
+            let out = await fs.readFile(path)
+            playlistCache.set(path, out)
+            let pl = hls.parse(out.toString()) as MediaPlaylist
+            console.log(pl.segments[0])
+        } else if (path.endsWith(".ts")) { // MPEG-2 Segment
+
+        }
+    } else if (e == "change" && path.endsWith(".m3u8")) {
+        let oldFile = playlistCache.get(path)
+        let newFile = await fs.readFile(path)
+        
+        playlistCache.set(path, newFile)
+        const diff = newFile?.subarray(oldFile?.length).toString()
+    }
 })
