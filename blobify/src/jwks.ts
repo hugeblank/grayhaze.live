aed53c606f8ae0dacf3fd317deae53a9e7c54852
hugeblank <hugeblank@gmail.com>
2024-12-11 23:23:20 -0800

    chore(blobify): Tweak JWK file a hair. todo: remove

--- blobify/src/jwks.ts
+++ blobify/src/jwks.ts
@@ -3,12 +3,18 @@ import fs from 'fs/promises'
 import { createHash } from 'crypto'
 import dotenv from 'dotenv'
 dotenv.config()
-// Just a little something to generate a jwk using the pkey passed in from the environment
+// Just a little something to generate a jwk
 export async function getJWK() {
-    const pkey = (await fs.readFile(process.env.PRIVATE_KEY_PATH as string))
+    const pkey = (await fs.readFile("rs256.pem"))
     return await JoseKey.fromImportable(pkey.toString(), createHash("SHA256").update(pkey).digest('hex'))
 }
 
 export async function showJWKJSON(key: JoseKey) {
     console.log(JSON.stringify({ keys: [key.publicJwk] }))
 }
+
+async function main() {
+    await showJWKJSON(await getJWK())
+}
+
+main()
