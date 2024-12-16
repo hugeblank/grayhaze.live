2d6af57d6fd17276edbe29cf2681d9c2aa6b65ee
hugeblank <hugeblank@gmail.com>
2024-12-16 01:48:27 -0800

    fix(affront): really bad fix to get around svelte's fetch function letting me down

--- affront/src/routes/+layout.ts
+++ affront/src/routes/+layout.ts
@@ -1,10 +1,10 @@
 import { lexicons } from "@atproto/api"
 import { isValidLexiconDoc, parseLexiconDoc } from "@atproto/lexicon"
 
-export const load = async ({ fetch, data }) => { 
-    const paths: any[] = (await (await fetch("/public/lexicons.json")).json())
+export const load = async ({ data}) => { 
+    const paths: any[] = await (await fetch("https://grayhaze.live/public/lexicons.json")).json()
     // Not too bothered about when these load in for now.
-    Promise.all(paths.map(async (doc, ind) => {
+    await Promise.all(paths.map(async (doc, ind) => {
         if (lexicons.get(doc["id"])) return
         if (!isValidLexiconDoc(doc)) console.error(`key ${ind} is not a valid LexiconDoc`)
         let ldoc = parseLexiconDoc(doc)
