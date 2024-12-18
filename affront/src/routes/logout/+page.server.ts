8d847c56cdb9bcb2904aa1a676d511a8a99a75ec
hugeblank <hugeblank@gmail.com>
2024-12-18 15:29:05 -0800

    fix(affront): remove session cookie when logging out

--- affront/src/routes/logout/+page.server.ts
+++ affront/src/routes/logout/+page.server.ts
@@ -1,10 +1,13 @@
-import type { LocalSession } from "$lib/session.js"
+import { localSessionStore, type LocalSession } from "$lib/session.js"
 import type { OAuthSession } from "@atproto/oauth-client-node"
 import { redirect } from "@sveltejs/kit"
 
-export const load = async ({ locals }) => {
+export const load = async ({ locals, cookies }) => {
     const l = locals as LocalSession
     const session = l.user?.agent.sessionManager as OAuthSession
     session.signOut()
+    const nkey = cookies.get("session")
+    if (nkey) await localSessionStore.del(nkey)
+    cookies.delete("session", { path: "/" })
     redirect(302, "/")
 }
