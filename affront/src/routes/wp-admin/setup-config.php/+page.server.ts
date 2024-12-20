bac0e2c2edab565e13a80837d97840b388e47510
hugeblank <hugeblank@gmail.com>
2024-12-20 14:59:33 -0800

    fix(affront): do the redirect better

--- affront/src/routes/wp-admin/setup-config.php/+page.server.ts
+++ affront/src/routes/wp-admin/setup-config.php/+page.server.ts
@@ -1,3 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
-redirect(308, "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUjcmljayBhc3RsZXkgbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D")
+export const load = async () => {
+    redirect(308, "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUjcmljayBhc3RsZXkgbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D")
+}
