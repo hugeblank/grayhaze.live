3cd57f432d78c90525b752a5fcc4a9edd2ad59fb
hugeblank <hugeblank@gmail.com>
2024-12-30 20:48:48 -0800

    test(affront): add really dumb debug

--- affront/src/routes/oauth/callback/+page.server.ts
+++ affront/src/routes/oauth/callback/+page.server.ts
@@ -6,11 +6,17 @@ import { Agent } from "@atproto/api";
 import { grayhazeAgent } from "$lib/Merged";
 
 export const load: PageServerLoad = async ({ cookies, url, fetch }) => {
+    console.log("a")
     const { session } = await client!.callback(url.searchParams)
+    console.log("b")
     const nkey = generateSessionToken()
+    console.log("c")
 	localSessionStore.set(nkey, session.did)
+    console.log("d")
     cookies.set("session", nkey, { path: "/" })
+    console.log("e")
     const user = await ATPUser.fromDID(session.did, fetch, grayhazeAgent(session))
+    console.log("f")
     try {
         await user.agent.live.grayhaze.actor.channel.get({
             repo: user.did,
@@ -23,5 +29,6 @@ export const load: PageServerLoad = async ({ cookies, url, fetch }) => {
         }, {})
         console.log(`created channel ${channel.uri}`)
     }
+    console.log("g")
     redirect(302, "/")
 }
